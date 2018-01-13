fcb5f3698387b5083df15fd65503630ed1fd6564
Matthew Casperson <matthewcasperson@gmail.com>
2018-01-13 13:37:02 +1000

    Added support for WildFly Swarm

---
+++ demo/swarm.sh
@@ -0,0 +1,2 @@
+ wget https://repo1.maven.org/maven2/org/wildfly/swarm/swarmtool/2017.12.1/swarmtool-2017.12.1-standalone.jar
+ /Library/Java/JavaVirtualMachines/jdk1.8.0_151.jdk/Contents/Home/bin/java -jar swarmtool-2017.12.1-standalone.jar -d com.h2database:h2:1.4.196 target/ticket-monster.war

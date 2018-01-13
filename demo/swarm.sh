8d35c77b8f46edec4e901c680ee29b1d6feb5461
Matthew Casperson <matthewcasperson@gmail.com>
2018-01-13 14:17:28 +1000

    Added some commands for pushing to Heroku

--- demo/swarm.sh
+++ demo/swarm.sh
@@ -2,3 +2,5 @@
     wget https://repo1.maven.org/maven2/org/wildfly/swarm/swarmtool/2017.12.1/swarmtool-2017.12.1-standalone.jar
  fi
  /Library/Java/JavaVirtualMachines/jdk1.8.0_151.jdk/Contents/Home/bin/java -jar swarmtool-2017.12.1-standalone.jar -d com.h2database:h2:1.4.196 target/ticket-monster.war
+ heroku deploy:jar ticket-monster-swarm.jar --app ticket-monster --options -Dswarm.http.port=$PORT
+ heroku logs --app ticket-monster --tail

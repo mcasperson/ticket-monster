9ce871844b40a38c53338caa7cd8ed6a8a6c5b64
Matthew Casperson <matthewcasperson@gmail.com>
2018-09-29 14:31:28 +1000

    Added xml dependency to build on Java 9+, and updated to the thorntail swarmtool.

--- demo/swarm.sh
+++ demo/swarm.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 if [ ! -f swarmtool-2018.5.0-standalone.jar ]; then
-   wget https://repo1.maven.org/maven2/org/wildfly/swarm/swarmtool/2018.5.0/swarmtool-2018.5.0-standalone.jar
+   wget https://repo1.maven.org/maven2/io/thorntail/swarmtool/2.2.0.Final/swarmtool-2.2.0.Final-standalone.jar
 fi
-/Library/Java/JavaVirtualMachines/jdk1.8.0_144.jdk/Contents/Home/bin/java -jar swarmtool-2018.5.0-standalone.jar -d com.h2database:h2:1.4.196 -d mysql:mysql-connector-java:5.1.45 target/ticket-monster.war
+/Library/Java/JavaVirtualMachines/jdk1.8.0_144.jdk/Contents/Home/bin/java -jar swarmtool-2.2.0.Final-standalone.jar -d com.h2database:h2:1.4.196 -d mysql:mysql-connector-java:5.1.45 target/ticket-monster.war
 heroku config:set --app ticket-monster JAVA_OPTS="-XX:+UseCompressedOops -Xmx128m -Xss228k -XX:MaxMetaspaceSize=128m"
 heroku deploy:jar ticket-monster-swarm.jar --app ticket-monster --options '-Dswarm.http.port=$PORT'
 heroku logs --app ticket-monster --tail
